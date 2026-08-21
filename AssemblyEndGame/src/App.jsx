b3eef2f682d7a06abf40f10d5adacd320f82dadf
vikram1931 <34970538+vikram1931@users.noreply.github.com>
2026-08-21 13:19:02 +0530

    added a section  and made some css changes for status on section

--- AssemblyEndGame/src/App.jsx
+++ AssemblyEndGame/src/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import Header from "./Header";
-export default function App() {
+export default function AssemblyEndgame() {
   return (
     <main>
-      Game goes here
-      <Header />
+      <header>
+        <h1>ASSEMBLY:ENDGAME</h1>
+        <p>
+          {" "}
+          Guess the word in under 8 attempts to keep the programming world safe
+          from Assembly!{" "}
+        </p>
+      </header>
+      <section className="game-status">
+        <h2>you win</h2>
+        <p>Well done !🎉</p>
+      </section>
     </main>
   );
 }
