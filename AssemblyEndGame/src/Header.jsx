12c9fd90edc01edc71f8b7ee3ed0213d8cd9a695
vikram1931 <34970538+vikram1931@users.noreply.github.com>
2026-08-21 11:36:19 +0530

    Adding the game title

---
+++ AssemblyEndGame/src/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <h1>ASSEMBLY:ENDGAME</h1>
+
+      <p>
+        {" "}
+        Guess the word in under 8 attempts to keep the programming world safe
+        from Assembly!{" "}
+      </p>
+    </div>
+  );
+}
