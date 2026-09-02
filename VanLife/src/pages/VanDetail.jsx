71e9e5e858653d26bd1413152248d197120e0bba
vikram1931 <34970538+vikram1931@users.noreply.github.com>
2026-09-02 22:15:05 +0530

    set a condition to show the details based on the fetched url

--- VanLife/src/pages/VanDetail.jsx
+++ VanLife/src/pages/VanDetail.jsx
@@ -3,13 +3,32 @@ import { useParams } from "react-router-dom";
 
 export default function VanDetail() {
   const params = useParams();
-  console.log(params);
-  // useParams brought the id from the route
 
+  // useParams brought the id from the route
+  const [van, setVan] = React.useState(null);
   useEffect(() => {
     fetch(`/api/vans/${params.id}`)
       .then((response) => response.json())
-      .then((json) => console.log(json));
+      .then((json) => setVan(json.vans));
   }, [params.id]);
-  return <h1>Van detail page</h1>;
+  return (
+    <div className="van-detail-container">
+      {van ? (
+        <div className="van-detail">
+          <img src={van.imageUrl} alt="" />
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
+          <h2>{van.name}</h2>
+          <p className="van-price">
+            <span>${van.price}</span>/day
+          </p>
+          <p>{van.description}</p>
+          <button type="button" className="link-button">
+            Rent this van
+          </button>
+        </div>
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </div>
+  );
 }
