73b4ce572b50de9f8c05558b5c04645de684a4f6
vikram1931 <34970538+vikram1931@users.noreply.github.com>
2026-09-02 20:24:30 +0530

    fetching van data and displaying

--- VanLife/src/pages/Vans.jsx
+++ VanLife/src/pages/Vans.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Vans() {
-  return <h1>Vans page</h1>;
+  const [vans, setVans] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/vans")
+      .then((response) => response.json())
+      .then((json) => setVans(json.vans));
+  }, []);
+
+  const vanElements = vans.map((van) => (
+    <div key={van.id} className="van-tile">
+      <img src={van.imageUrl} alt="" />
+      <div className="van-info">
+        <h3>{van.name}</h3>
+        <p>
+          ${van.price}
+          <span>/day</span>
+        </p>
+      </div>
+      <i className={`van-type ${van.type} selected`}>{van.type}</i>
+    </div>
+  ));
+  return (
+    <div className="van-list-container">
+      <h1>Explore our van options</h1>
+      <div className="van-list">{vanElements}</div>
+    </div>
+  );
 }
