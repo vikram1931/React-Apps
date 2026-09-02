0f4eb74e7656612187e99a2856818e859012fa75
vikram1931 <34970538+vikram1931@users.noreply.github.com>
2026-09-02 16:43:00 +0530

    vans component, Route and link added to it

--- VanLife/src/index.jsx
+++ VanLife/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-
+import Vans from "./pages/Vans";
 function App() {
   return (
     <BrowserRouter>
@@ -13,11 +13,13 @@ function App() {
         </Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/Vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/Vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ VanLife/src/pages/Vans.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Vans() {
+  return <h1>Vans page</h1>;
+}
