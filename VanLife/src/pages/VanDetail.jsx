8409ecdc8a70e763730afb49f4439cf11cb3dd67
vikram1931 <34970538+vikram1931@users.noreply.github.com>
2026-09-02 21:48:35 +0530

    using useParams() from router-dom and fetching the id and using it , which was sent from Route path

--- VanLife/src/pages/VanDetail.jsx
+++ VanLife/src/pages/VanDetail.jsx
@@ -1,5 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 export default function VanDetail() {
+  const params = useParams();
+  console.log(params);
+  // useParams brought the id from the route
+
+  useEffect(() => {
+    fetch(`/api/vans/${params.id}`)
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }, [params.id]);
   return <h1>Van detail page</h1>;
 }
