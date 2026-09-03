2aaab6b25285aae2e315e2defd8c90ef50dc387c
vikram1931 <34970538+vikram1931@users.noreply.github.com>
2026-09-03 18:50:36 +0530

    relative path

--- VanLife/src/index.jsx
+++ VanLife/src/index.jsx
@@ -17,13 +17,13 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/Vans" element={<Vans />} />
-          <Route path="/Vans/:id" element={<VanDetail />} />
-          <Route element={<HostLayout />}>
-            <Route path="/host" element={<Dashboard />} />
-            <Route path="/host/income" element={<Income />} />
-            <Route path="/host/reviews" element={<Reviews />} />
+          <Route path="about" element={<About />} />
+          <Route path="Vans" element={<Vans />} />
+          <Route path="Vans/:id" element={<VanDetail />} />
+          <Route path="host" element={<HostLayout />}>
+            <Route path="host" element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
         </Route>
       </Routes>
