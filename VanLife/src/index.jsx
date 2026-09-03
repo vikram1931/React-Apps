8ac8d693be9ac7bff27f4e411a7b69a1d664eeab
vikram1931 <34970538+vikram1931@users.noreply.github.com>
2026-09-03 18:42:04 +0530

    creating the host layout

--- VanLife/src/index.jsx
+++ VanLife/src/index.jsx
@@ -10,6 +10,7 @@ import Layout from "./components/Layout";
 import Dashboard from "./pages/Host/Dashboard";
 import Income from "./pages/Host/Income";
 import Reviews from "./pages/Host/Reviews";
+import HostLayout from "./pages/Host/HostLayout";
 function App() {
   return (
     <BrowserRouter>
@@ -19,7 +20,8 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/Vans" element={<Vans />} />
           <Route path="/Vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<Dashboard />}>
+          <Route element={<HostLayout />}>
+            <Route path="/host" element={<Dashboard />} />
             <Route path="/host/income" element={<Income />} />
             <Route path="/host/reviews" element={<Reviews />} />
           </Route>
