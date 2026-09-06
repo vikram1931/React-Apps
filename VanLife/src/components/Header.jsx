49653ec26fe3a60c054467bf943d9ea942860886
vikram1931 <34970538+vikram1931@users.noreply.github.com>
2026-09-06 18:33:53 +0530

    Added NavLink and its function and destructured isActive property from it and footer

--- VanLife/src/components/Header.jsx
+++ VanLife/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 export default function Header() {
   return (
     <header>
@@ -7,9 +7,21 @@ export default function Header() {
         #VanLife
       </Link>
       <nav>
-        <Link to="/host">Host</Link>
-        <Link to="/about">About</Link>
-        <Link to="/Vans">Vans</Link>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+          to="/host">
+          Host
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+          to="/about">
+          About
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+          to="/Vans">
+          Vans
+        </NavLink>
       </nav>
     </header>
   );
--- VanLife/src/components/Layout.jsx
+++ VanLife/src/components/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Footer from "./Footer";
 export default function Layout() {
   return (
-    <>
+    <div className="site-wrapper">
       <Header />
       <Outlet />
-    </>
+      <Footer />
+    </div>
   );
 }
