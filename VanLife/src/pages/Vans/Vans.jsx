c229921c8102cb5ba5f17c0c0ae368fe11c46389
vikram1931 <34970538+vikram1931@users.noreply.github.com>
2026-09-08 21:13:48 +0530

    filter the vans with Links

--- VanLife/src/pages/Vans/Vans.jsx
+++ VanLife/src/pages/Vans/Vans.jsx
@@ -34,6 +34,20 @@ export default function Vans() {
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
+      <div className="van-list-container">
+        <Link to="?type=simple" className="van-type simple">
+          Simple
+        </Link>
+        <Link to="?type=luxury" className="van-type luxury">
+          Luxury{" "}
+        </Link>
+        <Link to="?type=rugged" className="van-type rugged">
+          Rugged
+        </Link>
+        <Link to="." className="van-type clear-filters">
+          clear
+        </Link>
+      </div>
       <div className="van-list">{vanElements}</div>
     </div>
   );
