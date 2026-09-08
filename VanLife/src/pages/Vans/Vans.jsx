83b48279a2985f09bc83c635d0b5c1a4867d646d
vikram1931 <34970538+vikram1931@users.noreply.github.com>
2026-09-08 21:32:14 +0530

    changed Links to buttons to filter the vans with a setter function

--- VanLife/src/pages/Vans/Vans.jsx
+++ VanLife/src/pages/Vans/Vans.jsx
@@ -35,18 +35,26 @@ export default function Vans() {
     <div className="van-list-container">
       <h1>Explore our van options</h1>
       <div className="van-list-container">
-        <Link to="?type=simple" className="van-type simple">
+        <button
+          onClick={() => setSearchParams({ type: "simple" })}
+          className="van-type simple">
           Simple
-        </Link>
-        <Link to="?type=luxury" className="van-type luxury">
+        </button>
+        <button
+          onClick={() => setSearchParams({ type: "luxury" })}
+          className="van-type luxury">
           Luxury{" "}
-        </Link>
-        <Link to="?type=rugged" className="van-type rugged">
+        </button>
+        <button
+          onClick={() => setSearchParams({ type: "rugged" })}
+          className="van-type rugged">
           Rugged
-        </Link>
-        <Link to="." className="van-type clear-filters">
+        </button>
+        <button
+          onClick={() => setSearchParams({})}
+          className="van-type clear-filters">
           clear
-        </Link>
+        </button>
       </div>
       <div className="van-list">{vanElements}</div>
     </div>
