8f6eaf67929cd6f3fc20aca96d54ee22cd138630
vikram1931 <34970538+vikram1931@users.noreply.github.com>
2026-09-11 16:36:34 +0530

    accessibility addition

--- VanLife/src/pages/Vans/Vans.jsx
+++ VanLife/src/pages/Vans/Vans.jsx
@@ -47,10 +47,10 @@ export default function Vans() {
   ));
 
   if (loading) {
-    return <h1>Loading ...</h1>;
+    return <h1 aria-live="polite">Loading ...</h1>;
   }
   if (error) {
-    return <h1> There was an error: {error.message} </h1>;
+    return <h1 aria-live="assertive"> There was an error: {error.message} </h1>;
   }
 
   return (
