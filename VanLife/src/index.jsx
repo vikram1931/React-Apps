5bd849db1d70c054f8e729c2e9745e6a8f9033dd
vikram1931 <34970538+vikram1931@users.noreply.github.com>
2026-09-11 14:17:34 +0530

    created 404 page

--- VanLife/src/index.jsx
+++ VanLife/src/index.jsx
@@ -16,11 +16,13 @@ import HostVanDetail from "./pages/Host/HostVanDetail";
 import HostVanInfo from "./pages/Host/HostVanInfo";
 import HostVanPricing from "./pages/Host/HostVanPricing";
 import HostVanPhotos from "./pages/Host/HostVanPhotos";
+import NotFound from "./pages/NotFound";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route path="*" element={<NotFound />} />
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="Vans">
